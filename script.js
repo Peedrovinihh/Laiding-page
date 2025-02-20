2a3db5603e71f943125f6c299dd63f27b1598db5
Pedro <peedrovinihh@gmail.com>
2025-02-20 16:33:20 -0300

    lading page html

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você pode adicionar código para enviar os dados do formulário para um servidor, se necessário.
+});
